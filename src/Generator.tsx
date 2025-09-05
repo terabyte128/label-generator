0f58868cec95fe5032b1292cffceee769f2eda18
Sam Wolfson <terabyte128@gmail.com>
2025-09-04 22:34:32 -0700

    add repeat count

--- src/Generator.tsx
+++ src/Generator.tsx
@@ -2,31 +2,30 @@ import { useState } from "react";
 import { generateLabels } from "./generate";
 import {
   Button,
-  Checkbox,
   Group,
-  Input,
+  NumberInput,
   Stack,
+  Text,
   Textarea,
   TextInput,
   Title,
 } from "@mantine/core";
 import { useHotkeys } from "@mantine/hooks";
+import { IconCheck, IconCommand, IconTagFilled } from "@tabler/icons-react";
 
 export default function Generator() {
   const [itemIds, setItemIds] = useState<string>("");
   const [fileData, setFileData] = useState<string | undefined>(undefined);
-  const [skipFirstChecked, setSkipFirstChecked] = useState<boolean>(false);
-  const [skipFirst, setSkipFirst] = useState<number | undefined>(undefined);
+  const [skipFirst, setSkipFirst] = useState<number>(0);
   const [isGenerating, setIsGenerating] = useState<boolean>(false);
   const [prefix, setPrefix] = useState<string>("CHTL");
+  const [repeatCount, setRepeatCount] = useState<number>(1);
 
   const fillOutIds = (inputText: string): number[] => {
     const ids: number[] = [];
 
-    if (skipFirstChecked && skipFirst) {
-      // Skip the first N labels
-      ids.push(...Array.from({ length: skipFirst }, () => -1));
-    }
+    // Skip the first N labels
+    ids.push(...Array.from({ length: skipFirst }, () => -1));
 
     const tokens = inputText
       .split(/[\s,]+/)
@@ -41,7 +40,9 @@ export default function Generator() {
         if (!Number.isNaN(start) && !Number.isNaN(end)) {
           const step = start <= end ? 1 : -1;
           for (let n = start; step > 0 ? n <= end : n >= end; n += step) {
-            ids.push(n);
+            for (let i = 0; i < repeatCount; i++) {
+              ids.push(n);
+            }
           }
         }
       } else {
@@ -67,54 +68,62 @@ export default function Generator() {
   return (
     <Group grow m="md" align="flex-start">
       <Stack>
-        <Title>Label Generator</Title>
+        <Title>
+          <IconTagFilled /> Label Generator
+        </Title>
         <TextInput
           label="Prefix"
           value={prefix}
           onChange={(e) => setPrefix(e.target.value)}
         />
+        <NumberInput
+          label="Repeat Count"
+          description="Number of times to repeat each ID"
+          value={repeatCount}
+          min={1}
+          hideControls
+          onChange={(value) => {
+            if (typeof value === "number") setRepeatCount(value);
+          }}
+        />
         <Textarea
           label="Item IDs"
+          description="Comma-separated list of item IDs or ranges"
           placeholder="e.g., 1-10,17,23"
           value={itemIds}
           onChange={(e) => setItemIds(e.target.value)}
         />
-        <Group align="center">
-          <Checkbox
-            checked={skipFirstChecked}
-            onChange={(e) => setSkipFirstChecked(e.currentTarget.checked)}
-          />
-          Skip first{" "}
-          <Input
-            type="number"
-            size="xs"
-            value={skipFirst}
-            onChange={(e) =>
-              setSkipFirst(parseInt(e.target.value, 10) || undefined)
-            }
-          />
-          labels
-        </Group>
-
+        <NumberInput
+          label="Skip First"
+          description="Number of labels to skip on sheet"
+          value={skipFirst}
+          min={0}
+          hideControls
+          onChange={(value) => {
+            if (typeof value === "number") setSkipFirst(value);
+          }}
+        />
         <Button
           onClick={onGenerate}
           loading={isGenerating}
           disabled={isGenerating}
+          leftSection={<IconCheck />}
         >
           Generate Labels
         </Button>
       </Stack>
-      <object
-        data={fileData}
-        type="application/pdf"
-        width="100%"
-        height="800px"
-      >
-        <p>
-          Your browser does not support PDFs.{" "}
-          <a href={fileData}>Download the PDF</a>.
-        </p>
-      </object>
+      <div>
+        <object
+          data={fileData}
+          type="application/pdf"
+          style={{ width: "100%", height: "calc(100vh - 50px)" }}
+        >
+          <p>
+            Your browser does not support PDFs.{" "}
+            <a href={fileData}>Download the PDF</a>.
+          </p>
+        </object>
+      </div>
     </Group>
   );
 }
